5883054b5c0f2439acc46592e645c0679025dba7
robfrkiller <robfrkiller@gmail.com>
2023-03-04 19:45:34 +0800

    chore: remove dotenv

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
         {
             name: 'RSSHub',
             script: 'lib/index.js',
-            log_date_format: 'YYYY-MM-DD-HH:mm:ss'
-        }
-    ]
-}
+            log_date_format: 'YYYY-MM-DD-HH:mm:ss',
+            node_args: '--env-file=.env',
+        },
+    ],
+};
